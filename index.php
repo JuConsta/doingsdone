1bf83e1c2eb928afd1a69ac6cdb9c2cd825980d5
Julia <juconstant@yandex.ru>
2019-10-27 11:46:40 +0300

    Убирает опечатку

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $show_complete_tasks = rand(0, 1);
 
                 <div class="main-header__side-item user-menu">
                     <div class="user-menu__data">
-                        <p>>Юлия</p>
+                        <p>Юлия</p>
 
                         <a href="#">Выйти</a>
                     </div>
